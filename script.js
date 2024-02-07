56115df1b672e4ba5f352389c888097412062037
Bhavan48 <105624782+Bhavan48@users.noreply.github.com>
2024-02-07 21:11:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 var btc=document.getElementById("bitcoin");
 var eth=document.getElementById("ethereum");
-var doge=document.getElementById("dogecoin");
+var dot=document.getElementById("polkadot");
+var ada=document.getElementById("cardano");
+var sol=document.getElementById("solana");
+var link=document.getElementById("chainlink");
 
 var settings={
     "async":true,
     "scrossDomain":true,
-    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cpolkadot%2Ccardano%2Csolana%2Cchainlink&vs_currencies=usd",
     "method":"GET",
     "headers":{}
 }
 $.ajax(settings).done(function(response){
   btc.innerHTML=response.bitcoin.usd;
   eth.innerHTML=response.ethereum.usd;
-  doge.innerHTML=response.dogecoin.usd;
-
+  dot.innerHTML=response.polkadot.usd;
+  ada.innerHTML=response.cardano.usd;
+  sol.innerHTML=response.solana.usd;
+  link.innerHTML=response.chainlink.usd;
 });
